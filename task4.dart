ec91c6d790919512be2f70aa4da11485f6dc81bf
Kasmi Thapa <kasmithapa00@gmail.com>
2024-11-18 09:10:02 +0545

    correction

---
+++ task4.dart
@@ -0,0 +1,10 @@
+//sum of two numbers
+void main() {
+  int num1 = 10;
+  int num2 = 20;
+  print("Sum: ${add(num1, num2)}");
+}
+
+int add(int a, int b) {
+  return a + b;
+}
---
+++ task5.dart
@@ -0,0 +1,9 @@
+//Multiply two numbers
+void main() {
+  int result = multiply(3, 4);
+  print("The product is: $result");
+}
+
+int multiply(int a, int b) {
+  return a * b;
+}
